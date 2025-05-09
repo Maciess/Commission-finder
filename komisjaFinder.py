c949c5cb1de446b8ba17d6d4f4dbb40c97472c9f
Maciess <m4ciess@gmail.com>
2025-05-09 18:27:05 +0200

    Add all location from Wroclaw. Lable pins with commision number.

--- komisjaFinder.py
+++ komisjaFinder.py
@@ -5,32 +5,32 @@ import pandas as pd
 
 # List of addresses
 data = pd.read_excel("location.xlsx")
-data = data.head(10) # take 10 for test
-addresses = data["address"]
-
+# data = data.head(10)  # take 10 for test
 
 geolocator = Nominatim(user_agent="map_pinner")
 
 map_center = [51.107883, 17.038538]  # Set to Wroclaw
-map_obj = folium.Map(location=map_center, zoom_start=5)
+map_obj = folium.Map(location=map_center, zoom_start=10)
 
 counter = 0
 NUM_OF_RECORDS = data.shape[0]
 
-for address in addresses:
+for index, row in data.iterrows():
+    address = row["address"]
+    commsion_number = str(row["number"])
     address_stripped = address[address.find("ul.")+4:]
     location = geolocator.geocode(address_stripped)
     if location:
         folium.Marker(
             location=[location.latitude, location.longitude],
-            popup=address,
-            tooltip="Click for address"
+            popup=commsion_number + " " + address,
+            tooltip=commsion_number
         ).add_to(map_obj)
         counter += 1
         print(f"Found {counter} of {NUM_OF_RECORDS}")
     else:
         print(f"Could not geocode address: {address}")
-    time.sleep(0.2) # to be not banned 
+    time.sleep(1.01)  # to be not banned
 
 
 map_obj.save("map_with_pins.html")
